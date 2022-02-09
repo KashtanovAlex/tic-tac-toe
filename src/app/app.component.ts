4c489c839e78dc44b27e42db02e1a1aa971ed4a9
kashtanovalex <kashtanov.alex@inbox.ru>
2022-02-09 20:21:37 +0700

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'tic-tac-toe';
+  turnd = "нолик";
+  clicks(event: Event): void {
+    let elementId: string = (event.target as Element).id;
+    console.log(elementId);
+    if (this.turnd == "нолик")
+    {
+      (event.target as Element).textContent = "0";
+      this.turnd = "крестик";
+    }
+    else
+    {
+      (event.target as Element).textContent = "X";
+      this.turnd = "нолик";
+    }
+  }
+  newGame(): void {
+  }
 }
